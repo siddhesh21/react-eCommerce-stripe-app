f5bbde0d995872cd80141d79510db4cd00f1da9e
Siddhesh Kankekar <kankekas@tcd.ie>
2021-03-20 21:02:24 +0000

    Added commerce  Products + Shipping details

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import "./App.css";
+import { commerce } from "./lib/commerce";
 import { Products, NavBar } from "./components";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+
   return (
     <div className="app">
       <NavBar />
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -8,7 +8,7 @@ const products = [
     id: 1,
     name: "iPhone 12 Pro",
     description: "A14 Bionic Chip",
-    price: "$1099",
+    price: "€1,529.00",
     image:
       "https://images.unsplash.com/photo-1592750475338-74b7b21085ab?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=934&q=80",
   },
@@ -16,7 +16,7 @@ const products = [
     id: 2,
     name: "Macbook Pro",
     description: "Apple M1 Chip with 8‑Core CPU and GPU 512GB Storage",
-    price: "$1679",
+    price: "€1,679.00",
     image:
       "https://images.unsplash.com/photo-1612383401559-c32a290d6b17?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1025&q=80",
   },
@@ -24,7 +24,7 @@ const products = [
     id: 3,
     name: "Apple Mac Mini ",
     description: "Apple M1 Chip with 8‑Core CPU and GPU 512GB Storage",
-    price: "$1029",
+    price: "€1,029.00",
     image:
       "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mac-mini-202011-gallery-3?wid=2000&hei=1536&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1603755840000",
   },
