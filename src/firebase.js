ad9e5b7c5fe8534d8979537f4354ca11233b41ea
Siddhesh Kankekar <kankekas@tcd.ie>
2021-03-29 19:50:29 +0100

    Added and Deployed with Firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCXDX6s79whxWJrXxmNZxLYDdoaYTAUxio",
+  authDomain: "project-ecommerce-6ef06.firebaseapp.com",
+  projectId: "project-ecommerce-6ef06",
+  storageBucket: "project-ecommerce-6ef06.appspot.com",
+  messagingSenderId: "879390727659",
+  appId: "1:879390727659:web:ca535fa97929dc48734dae",
+  measurementId: "G-S4BT3XTZV8",
+};
